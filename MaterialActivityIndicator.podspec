0d2f448c72d02c312d26b58c1115ade430abf11f
Dominik Kapusta <dominik@kapusta.cc>
2019-10-16 00:00:31 +0200

    Add support for carthage, 0.0.7

--- MaterialActivityIndicator.podspec
+++ MaterialActivityIndicator.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'MaterialActivityIndicator'
-  s.version          = '0.0.6.3'
+  s.version          = '0.0.7'
   s.summary          = 'Material activity indicator'
   s.description      = <<-DESC
 Material activity indicator (Circular progress indicator)
@@ -16,6 +16,6 @@ Material activity indicator (Circular progress indicator)
   s.ios.deployment_target = '9.0'
   s.tvos.deployment_target = '9.0'
 
-  s.source_files = 'MaterialActivityIndicator/Classes/**/*'
+  s.source_files = 'Sources/**/*'
   s.frameworks = 'UIKit'
 end
---
+++ MaterialActivityIndicator/MaterialActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  MaterialActivityIndicator.h
+//  MaterialActivityIndicator
+//
+//  Created by Dominik Kapusta on 15/10/2019.
+//  Copyright © 2019 DonkeyRepublic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaterialActivityIndicator.
+FOUNDATION_EXPORT double MaterialActivityIndicatorVersionNumber;
+
+//! Project version string for MaterialActivityIndicator.
+FOUNDATION_EXPORT const unsigned char MaterialActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialActivityIndicator/PublicHeader.h>
+
+
